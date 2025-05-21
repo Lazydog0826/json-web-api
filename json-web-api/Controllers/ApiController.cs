1af00dee21ed75ce3a3c86fa5c82e38949c92812
hzc <2635174704@qq.com>
2025-05-21 20:08:24 +0800

    测试2

--- json-web-api/Controllers/ApiController.cs
+++ json-web-api/Controllers/ApiController.cs
@@ -5,21 +5,21 @@ using Yitter.IdGenerator;
 
 namespace json_web_api.Controllers
 {
-    public class ApiController(IMemoryCache _memoryCache) : ControllerBase
+    public class ApiController(IMemoryCache memoryCache) : ControllerBase
     {
         [HttpPost("AddJson")]
         public IActionResult AddJsonAsync([FromBody] AddJsonRequest param)
         {
             var key = YitIdHelper.NextId().ToString();
             param.Key = key;
-            _memoryCache.Set(key, param, TimeSpan.FromHours(param.Hour));
+            memoryCache.Set(key, param, TimeSpan.FromHours(param.Hour));
             return Ok(new { Key = key });
         }
 
         [HttpGet("GetJson")]
         public IActionResult GetJsonAsync([FromQuery] string key)
         {
-            var res = _memoryCache.Get<AddJsonRequest>(key);
+            var res = memoryCache.Get<AddJsonRequest>(key);
             return Ok(res ?? new AddJsonRequest());
         }
     }
